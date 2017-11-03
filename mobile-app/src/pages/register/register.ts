66e990e13021ad2fd581ac9fec1f8c5f59d9d78f
Pat Richeal <patrickricheal@gmail.com>
2017-11-03 19:46:55 -0400

    Added inputs and basic validation to register page

--- mobile-app/src/pages/register/register.ts
+++ mobile-app/src/pages/register/register.ts
@@ -8,6 +8,14 @@ import { NavController, Slides } from 'ionic-angular';
 export class RegisterPage implements OnInit {
   @ViewChild(Slides) slides: Slides;
 
+  first_name: String = "";
+  last_name: String = "";
+  email_address: String = "";
+  phone_number: String = "";
+  family_members: String = "";
+
+  error_message: String = "";
+
   constructor(public navCtrl: NavController) {}
 
   ngOnInit() {
@@ -16,7 +24,20 @@ export class RegisterPage implements OnInit {
 
   next() {
     this.slides.lockSwipes(false);
-    this.slides.slideNext();
+
+    if (this.slides.getActiveIndex() == 0) {
+      // do necessary calls to validate access code and then continue
+      this.slides.slideNext();
+    }
+
     this.slides.lockSwipes(true);
+  }
+
+  signUp() {
+    if (this.first_name && this.last_name && this.email_address && this.phone_number && this.family_members) {
+      //try to signup
+    } else {
+      this.error_message = "Please fill out all fields";
+    }
   }
 }
