5d2a410a3db8cb407d1783fa98f81b073829715f
Pat Richeal <patrickricheal@gmail.com>
2017-11-03 19:09:17 -0400

    Set main theme to android

--- mobile-app/src/app/app.module.ts
+++ mobile-app/src/app/app.module.ts
@@ -20,7 +20,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp, {
+      mode: 'md'
+    })
   ],
   bootstrap: [IonicApp],
   entryComponents: [
