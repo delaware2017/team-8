d94d1ff642f57a64eda3ad3a9a4eebb6d25efbf5
Kaveh Khorram <kavehkhorram@Kavehs-MacBook-Pro.local>
2017-11-03 18:35:34 -0400

    added body parser

--- server/index.js
+++ server/index.js
@@ -2,8 +2,13 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-    console.log("request recieved.");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("account/registration", (req, res) => {
+    // Registers a new user.
 })
 
 app.listen(process.env.PORT || 3000);
