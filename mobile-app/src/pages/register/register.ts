8f93d6bfa842a52aa9c993fa105917cf85a8c85e
Pat Richeal <patrickricheal@gmail.com>
2017-11-03 19:05:03 -0400

    Added mobile-app project

---
+++ mobile-app/src/pages/register/register.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-register',
+  templateUrl: 'register.html'
+})
+export class RegisterPage {
+
+  constructor(public navCtrl: NavController) {}
+}
