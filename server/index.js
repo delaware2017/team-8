c538c7c07b6d33664a0beb80363b7e7637f2ef53
Kaveh Khorram <kavehkhorram@Kavehs-MacBook-Pro.local>
2017-11-03 18:38:04 -0400

    switched to POST requests.

--- server/index.js
+++ server/index.js
@@ -7,8 +7,20 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("account/registration", (req, res) => {
+app.post("account/registration", (req, res) => {
     // Registers a new user.
-})
+});
+
+app.post("account/update", (req, res) => {
+    // Updates an account
+});
+
+app.post("account/remove", (req, res) => {
+    // Remove an account (for some reason?)
+});
+
+app.post("account/update/balance", (req, res) => {
+    // Update a user balance
+});
 
 app.listen(process.env.PORT || 3000);
