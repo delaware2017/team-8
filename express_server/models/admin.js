2585b9f37f5c82340229eeda32f153844c2f63fe
David <dchang2@swarthmore.edu>
2017-11-03 20:52:54 -0400

    still working on login and registration

--- express_server/models/admin.js
+++ express_server/models/admin.js
@@ -4,10 +4,16 @@ var mongoose = require('mongoose');
 var adminSchema = mongoose.Schema({
   username: {type: String, required: true},
   password: {type: String, required: true},
-  listOfUsers: [{type: mongoose.Schema.Types.ObjectID, ref: 'User'}]
+  listOfUsers: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}]
 })
 
 // Define methods ====================================================================================================================================================================
+adminSchema.methods.hashPassword = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
 
+adminSchema.methods.validatePassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+};
 // Export schema =====================================================================================================================================================================
 module.exports = mongoose.model('Admin', adminSchema);
