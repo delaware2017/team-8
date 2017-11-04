f91208854561695a77fe0b60737d66c428524260
jessicazhou <itsjessicazhou@gmail.com>
2017-11-03 18:22:19 -0700

    google maps integration w ionic

--- express_server/routes/index.js
+++ express_server/routes/index.js
@@ -6,4 +6,23 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+var obj = []
+obj.push({name:'ACME Markets', address: '1401 N Dupont St' });
+obj.push({name: 'Giant', Address: '4301 Concord Pike'});
+
+
+.config (function$stateProvider, $urlRouterProvider){
+ $stateProvider
+ .state('map', {
+   
+ })
+});
+
+router.post('/googleMaps', (req, res) => {
+  {
+      
+  }
+})
+
 module.exports = router;
+ 
