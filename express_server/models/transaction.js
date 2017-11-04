6afb101ed1bdb91f099b12d0ed9f66b5b2c857b0
David <dchang2@swarthmore.edu>
2017-11-04 04:12:56 -0400

    added a new model

--- express_server/models/transaction.js
+++ express_server/models/transaction.js
@@ -1,12 +1,11 @@
 var mongoose = require('mongoose');
 
 // Define schema =====================================================================================================================================================================
-var codeSchema = mongoose.Schema({
-  code: {type: String, required: true},
-  user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-  admin: {type: mongoose.Schema.Types.ObjectId, ref: 'Admin'},
-  plan: {type: Number}
+var transactionSchema = mongoose.Schema({
+  amount: {type: String},
+  retailer: {type: String},
+  date: {type: Date, default: Date.now()}
 })
 
 // Export schema =====================================================================================================================================================================
-module.exports = mongoose.model('Code', codeSchema);
+module.exports = mongoose.model('Transaction', transactionSchema);
