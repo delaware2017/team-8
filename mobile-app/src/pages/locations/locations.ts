05c551c8830f79de0c3d8e629dc1ee7161540c18
Pat Richeal <patrickricheal@gmail.com>
2017-11-04 00:07:19 -0400

    Added maps to locations

--- mobile-app/src/pages/locations/locations.ts
+++ mobile-app/src/pages/locations/locations.ts
@@ -6,6 +6,17 @@ import { Component } from '@angular/core';
 })
 export class LocationsPage {
 
+  locations = [
+    {
+      name: "Clark's Farm Stand",
+      address: "11 N. Way St, Madison, WI 53703",
+      time_away: "10 mins",
+      distance_away: "2.6 miles",
+      lat: 39.277543,
+      lon: -74.579229
+    }
+  ];
+
   constructor() {}
 
 }
